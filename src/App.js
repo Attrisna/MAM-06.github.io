77cebc1ebdcae158553dfdb94fd3db10b85d6233
MAM <khotib.bul@gmail.com>
2022-12-01 09:45:05 +0700

    Main

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <p>
+          MAM <span className="heart">-</span> 06
+        </p>
+        <p className="small">
+          Student <code>Information</code> System
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://www.instagram.com/mam.06_/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Follow me
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
